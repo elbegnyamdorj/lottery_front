37eece8cb261435cf2972b7080e902e9ecf34885
Nyamdorj Elbeg <nyamaa@Nyamdorj.local>
2023-01-08 03:23:38 +0800

    f

--- src/components/search-box/index.jsx
+++ src/components/search-box/index.jsx
@@ -1,37 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './style.css';
 
 import RICIBs from 'react-individual-character-input-boxes';
 
-export default class SearchBox extends Component{
-  constructor(){
+export default class SearchBox extends Component {
+  constructor() {
     super();
-    
+
     this.state = JSON.parse(window.localStorage.getItem('state')) || {
-        count:0
-    }
+      count: 8,
+    };
     console.log(this.state.count);
   }
-  setState(state){
+  setState(state) {
     window.localStorage.setItem('state', JSON.stringify(state));
     super.setState(state);
   }
-  handleOutput(string){
-    console.log(string)
+  handleOutput(string) {
+    console.log(string);
   }
 
-
-    render () {
-
-        return (
-          <div> 
-            <RICIBs
-              amount={parseInt(this.state.count)}
-              handleOutputString={this.props.onSearch}
-              // handleOutputString={this.handleOutput}
-              inputRegExp={/^[A-Z0-9]$/}
-            />
-          </div>
-        )
-      }
+  render() {
+    return (
+      <div>
+        <RICIBs
+          amount={parseInt(this.state.count)}
+          handleOutputString={this.props.onSearch}
+          // handleOutputString={this.handleOutput}
+          inputRegExp={/^[A-Z0-9]$/}
+        />
+      </div>
+    );
+  }
 }
