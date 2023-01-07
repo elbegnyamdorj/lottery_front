b254358fa9e05019400dc3011b76657cb33d5f83
Nyamdorj Elbeg <nyamaa@Nyamdorj.local>
2023-01-08 02:08:15 +0800

    f

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,5 @@
 // const MAIN_URL = "http://43.231.115.58:81";
-const MAIN_URL = 'http://103.168.56.72:8000';
+const MAIN_URL = 'http://103.168.56.72:8080';
 
 export const getLotteryList = `${MAIN_URL}/lottery/`;
 export const postLotteryList = `${MAIN_URL}/lottery/`;
